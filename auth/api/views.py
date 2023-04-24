dff58e16346762b2aa63cd876c6d6867c2778570
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2023-04-24 22:33:37 +0530

    initial boilerplate

--- auth/api/views.py
+++ auth/api/views.py
@@ -5,3 +5,19 @@ from auth.app import db
 from auth.models.token_blocklist import TokenBlocklist
 from auth.models.user import User
 
+auth_blueprint = Blueprint("auth", __name__)
+
+class SignupAPI(MethodView):
+    pass
+
+class LoginAPI(MethodView):
+    pass
+
+class LogoutAPI(MethodView):
+    pass
+
+class UserAPI(MethodView):
+    pass
+
+
+
