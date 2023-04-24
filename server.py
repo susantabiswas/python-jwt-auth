5bc91b4a36cb8d222c95e8c36bfa4b5f9b4d6b2d
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2023-04-24 18:50:16 +0530

    renamed server name

--- server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-server = Flask(__name__)
-
-server.config.from_object("APP_SETTINGS")
-
-if __name__ == "__main__":
-    server.run(host='0.0.0.0', port=5000)
