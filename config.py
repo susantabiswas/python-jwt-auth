79b3cf8166666e0654b9b7a40bd151f55c977f32
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2023-04-24 18:48:36 +0530

    new configs added for sqlalchemy

--- config.py
+++ config.py
@@ -3,12 +3,15 @@ from os import environ, path
 
 APP_ENV_FILE = ".env"
 # Load the environment vars from the local env file
-base_dir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(base_dir, APP_ENV_FILE))
+BASE_DIR = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(BASE_DIR, APP_ENV_FILE))
 
 class BaseConfig:
     # Flask config
     SECRET_KEY = environ.get('SECRET_KEY')
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = environ('DATABASE_URI')
 
     # MySql config
     MYSQL_USER = environ.get("DB_USER")
@@ -17,8 +20,8 @@ class BaseConfig:
 
 class DevConfig(BaseConfig):
     FLASK_ENV = "development"
-    DEBUG = False
-    TESTINg = True
+    DEBUG = True
+    TESTING = True
 
     # MySQL
     MYSQL_HOST = "localhost"
