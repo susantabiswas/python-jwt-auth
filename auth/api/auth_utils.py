07a3c929f166419457f52d7d2f5523592fcd7cd2
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2023-04-25 16:07:29 +0530

    jwt related methods added

---
+++ auth/api/auth_utils.py
@@ -0,0 +1,58 @@
+from datetime import datetime, timedelta
+import jwt
+from auth.app import app
+
+def encode_jwt_token(user_id: str)->str:
+    """Creates a JWT auth token. Uses a symmetric algorithm for
+    signing the token.
+
+    Args:
+        user_id (str): Unique user id
+
+    Raises:
+        e: Exception
+
+    Returns:
+        str: Encrypted token signature
+    """
+    try:
+        curr_time = datetime.utcnow()
+
+        payload = {
+            'sub': user_id,
+            'iat': curr_time,
+            'exp': curr_time +  timedelta(days=0, minutes=1, seconds=0)
+        }
+        return jwt.encode(
+            payload,
+            app.config['SECRET_KEY'],
+            algorithm='HS256' # We will use a symmetric algorithm
+        )
+    except Exception as e:
+        raise e
+
+def decode_jwt_token(jwt_token: str)->str:
+    """Decodes a JWT token signature
+
+    Args:
+        jwt_token (str): JWT token
+
+    Raises:
+        InvalidTokenError: If jwt token is invalid
+        ExpiredSignatureError: If the jwt token is expired
+        e: Any other exception
+
+    Returns:
+        str: Unique user id
+    """
+    try:
+        payload = jwt.decode(
+            jwt_token,
+            app.config['SECRET_KEY'])
+        return payload['sub']
+    except jwt.InvalidTokenError:
+        return "Token Signature is invalid."
+    except jwt.ExpiredSignatureError:
+        return "Signature is expired. Please log in again to refresh"
+    except Exception as e:
+        raise e 
