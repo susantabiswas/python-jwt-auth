a02f674e6e05bee309619efb5b314af7a8e9bd2d
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2023-04-25 14:13:54 +0530

    added CORS

--- auth/app.py
+++ auth/app.py
@@ -5,16 +5,19 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from auth.config import export_environment_variables
+from flask_cors import CORS
 
 # Load the environment vars from the local env file
 export_environment_variables() 
-
 
 # Create the Flask server and load the configs from the configuration
 # file. Some of the settings in the configuration file are taken from
 # environment variables
 app = Flask(__name__)
-app.config.from_object(environ.get("APP_ENVIRONMENT"))
+app.config.from_object(
+    environ.get("APP_ENVIRONMENT",
+        "auth.config.DevConfig"))
+CORS(app)
 
 # Setup the objects for handling database migration.
 # If there is any schema change, this will apply the changes to
@@ -24,6 +27,10 @@ migrate = Migrate(app, db)
 
 from auth.models.token_blocklist import TokenBlocklist
 from auth.models.user import User
+
+
+from auth.api.views import auth_blueprint
+app.register_blueprint(auth_blueprint)
 
 if __name__ == "__main__":
     app.run(
