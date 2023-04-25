6e940a6feab38fbb2b99d3a04932f5b67a88554c
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2023-04-25 14:39:14 +0530

    signup api added

--- auth/api/views.py
+++ auth/api/views.py
@@ -8,7 +8,51 @@ from auth.models.user import User
 auth_blueprint = Blueprint("auth", __name__)
 
 class SignupAPI(MethodView):
-    pass
+    def post(self):
+        # Get the post data
+        data = request.get_json()
+
+        # Check if the required fields are present or not
+        email, password, name = None, None, None
+        try:
+            email = data['email']
+            password = data['password']
+            name = data['name']
+        except Exception as e:
+            response = {
+                'status' : 'failed',
+                'message': 'Required fields are missing.'
+            }
+            return make_response(jsonify(response)), 400
+
+        # Check if the user already exists
+        user = User.query.filter_by(email=email).first()
+
+        if user:
+            response = {
+                'status': 'failed',
+                'message': 'User already exists.'
+            }
+            return make_response(jsonify(response)), 202
+        else:
+            try:
+                user = User(email=email, password=password, name=name)
+                db.session.add(user)
+                db.session.commit()
+
+                # generate the JWT token
+                response = {
+                    'status': 'success',
+                    'message': 'User signed up successful.'
+                }
+                return make_response(jsonify(response)), 201
+            except:
+                response = {
+                    'status': 'failed',
+                    'message': 'Internal error'
+                }
+                return make_response(jsonify(response)), 500
+
 
 class LoginAPI(MethodView):
     pass
@@ -29,23 +73,23 @@ user_api = UserAPI.as_view('user_view')
 auth_blueprint.add_url_rule(
     '/auth/signup',
     view_func=signup_api,
-    methods=["GET"]
+    methods=["POST"]
 )
 
 auth_blueprint.add_url_rule(
-    'auth/login',
+    '/auth/login',
     view_func=login_api,
     methods=["POST"]
 )
 
 auth_blueprint.add_url_rule(
-    'auth/logout',
+    '/auth/logout',
     view_func=logout_api,
     methods=["POST"]
 )
 
 auth_blueprint.add_url_rule(
-    'auth/user',
+    '/auth/user',
     view_func=user_api,
     methods=["GET"]
 )
