ccfe3f0408126c73d77d6998647b580c058a856e
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2023-04-26 17:29:23 +0530

    formatting changes

--- auth/models/blocked_token.py
+++ auth/models/blocked_token.py
@@ -10,7 +10,7 @@ class BlockedToken(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     token = db.Column(db.String(500), unique=True, nullable=False)
     blocked_on = db.Column(db.DateTime, nullable=False)
-
+    
     def __init__(self, token):
         """Creates a new instance of TokenBlockList
 
@@ -21,4 +21,9 @@ class BlockedToken(db.Model):
         self.blocked_on = datetime.utcnow()
 
     def __repr__(self):
+        """String representation
+
+        Returns:
+            str: <id:, token: >
+        """
         return '<id: {}, token: {}>'.format(id, self.token)
