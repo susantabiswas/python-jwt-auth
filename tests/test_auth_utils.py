873663980d98fd62e6290e9663910c2ae009f55a
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2023-04-26 13:52:24 +0530

    tests for checking jwt validity

--- tests/test_auth_utils.py
+++ tests/test_auth_utils.py
@@ -18,9 +18,9 @@ class TestAuthUtils(TestCaseBase):
         """Test whether a valid jwt token can be decoded
         """
         jwt_token = encode_jwt_token(self.user_id)
-        decoded_user_id = decode_jwt_token(jwt_token=jwt_token)
-        self.assertEqual(self.user_id, decoded_user_id)
-        self.assertIsInstance(decoded_user_id, type(self.user_id))
+        decoded_jwt_payload = decode_jwt_token(jwt_token=jwt_token)
+        self.assertEqual(self.user_id, decoded_jwt_payload['sub'])
+        self.assertIsInstance(decoded_jwt_payload['sub'], type(self.user_id))
 
     def test_decode_jwt_token__invalid_case(self):
         """Test whether an invalid jwt token can be decoded
@@ -90,3 +90,75 @@ class TestAuthUtils(TestCaseBase):
         # Active JWT token should not be found as blocked
         self.assertFalse(is_jwt_blocked(active_jwt_token))
 
+    def test_is_valid_jwt__valid_case(self):
+        """Test whether a valid jwt is recognized as valid
+        """
+        jwt_token = encode_jwt_token(self.user_id)
+        is_valid, jwt_payload, err_message, status_code = is_valid_jwt(jwt_token)
+
+        self.assertTrue(is_valid)
+        self.assertIsNotNone(jwt_payload)
+        self.assertIsNone(err_message)
+        self.assertEqual(status_code, 200)
+
+    def test_is_valid_jwt__jwt_missing(self):
+        """Test whether a missing jwt is recognized as valid
+        """
+        is_valid, jwt_payload, err_message, status_code = is_valid_jwt(None)
+
+        self.assertFalse(is_valid)
+        self.assertIsNone(jwt_payload)
+        self.assertIsNotNone(err_message)
+        self.assertEqual(err_message, "Auth token missing")
+        self.assertEqual(status_code, 401)
+    
+    def test_is_valid_jwt__blocked_token(self):
+        """Test whether a blocked jwt is recognized as valid
+        """
+        jwt_token = encode_jwt_token(self.user_id)
+        block_jwt_token(jwt_token)
+        is_valid, jwt_payload, err_message, status_code = is_valid_jwt(jwt_token)
+
+        self.assertFalse(is_valid)
+        self.assertIsNone(jwt_payload)
+        self.assertIsNotNone(err_message)
+        self.assertEqual(err_message, "JWT token is blocked. Please login again")
+        self.assertEqual(status_code, 401)
+
+    def test_is_valid_jwt__invalid_jwt(self):
+        """Test whether an invalid jwt is recognized as valid
+        """
+        jwt_token = encode_jwt_token(self.user_id)
+        is_valid, jwt_payload, err_message, status_code = is_valid_jwt(jwt_token + "incorrect")
+
+        self.assertFalse(is_valid)
+        self.assertIsNone(jwt_payload)
+        self.assertIsNotNone(err_message)
+        self.assertEqual(err_message, "Token Signature doesn't match")
+        self.assertEqual(status_code, 401)
+
+    def test_is_valid_jwt__invalid_jwt(self):
+        """Test whether an expired jwt format is recognized as valid
+        """
+        jwt_token = encode_jwt_token(self.user_id)
+        
+        # wait for the token to expire
+        time.sleep(app.config['JWT_SECONDS'] + 1)
+        is_valid, jwt_payload, err_message, status_code = is_valid_jwt(jwt_token)
+
+        self.assertFalse(is_valid)
+        self.assertIsNone(jwt_payload)
+        self.assertIsNotNone(err_message)
+        self.assertEqual(err_message, "Signature is expired. Please log in again to refresh")
+        self.assertEqual(status_code, 401)
+    
+    def test_is_valid_jwt__invalid_jwt_format(self):
+        """Test whether an invalid jwt format is recognized as valid
+        """
+        is_valid, jwt_payload, err_message, status_code = is_valid_jwt(2231)
+
+        self.assertFalse(is_valid)
+        self.assertIsNone(jwt_payload)
+        self.assertIsNotNone(err_message)
+        self.assertEqual(err_message, "Invalid token signature")
+        self.assertEqual(status_code, 401)
