b91c72eb60c0f8e70c76224b30c6c3466d3e4ad4
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2023-04-26 23:13:50 +0530

    tests added for models

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,6 +5,8 @@ from tests.base import TestCaseBase
 from datetime import datetime
 class UserModelTest(TestCaseBase):
     def test_user_model(self):
+        """Tests the user model
+        """
         user = User(email="user@test.com",
             password="test",
             name="user")
@@ -18,8 +20,23 @@ class UserModelTest(TestCaseBase):
         self.assertTrue(user.password, retrieved_user.password)
         self.assertTrue(user.name, retrieved_user.name)
 
+    def test_user_repr(self):
+        user = User(email="user@test.com",
+            password="test",
+            name="user")
+        db.session.add(user)
+        db.session.commit()
+        
+        self.assertEqual(
+            user.__repr__(),
+            '<Id: {}, Name: {}>'.format(user.id, user.name))
+
+
+
 class BlockedTokenModelTest(TestCaseBase):
     def test_user_model(self):
+        """Tests the BlockToken model
+        """
         blocked_token = BlockedToken(token="sadasdsadasd", token_expiry=datetime.utcnow())
         db.session.add(blocked_token)
         db.session.commit()
@@ -28,3 +45,12 @@ class BlockedTokenModelTest(TestCaseBase):
         retrieved_blocked_token = BlockedToken.query.filter_by(token=blocked_token.token).first()
 
         self.assertTrue(blocked_token.token, retrieved_blocked_token.token)
+
+    def test_user_repr(self):
+        blocked_token = BlockedToken(token="sadasdsadasd", token_expiry=datetime.utcnow())
+        db.session.add(blocked_token)
+        db.session.commit()
+        
+        self.assertEqual(
+            blocked_token.__repr__(),
+            '<Id: {}, Token: {}>'.format(blocked_token.id, blocked_token.token))
