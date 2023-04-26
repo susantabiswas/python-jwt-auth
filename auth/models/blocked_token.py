6cb4ae87309595856979fd31c62e98c46eddc146
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2023-04-26 19:15:08 +0530

    new column expiry time added to BlockedToken model

--- auth/models/blocked_token.py
+++ auth/models/blocked_token.py
@@ -1,8 +1,9 @@
 
 from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
+
+from auth.api.auth_utils import decode_jwt_token
 from auth.app import db
-from datetime import datetime
+
 
 class BlockedToken(db.Model):
     """Represents the schema to save the blocked token related information.
@@ -10,6 +11,7 @@ class BlockedToken(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     token = db.Column(db.String(500), unique=True, nullable=False)
     blocked_on = db.Column(db.DateTime, nullable=False)
+    expiry = db.Column(db.Datetime, nullable=False)
     
     def __init__(self, token):
         """Creates a new instance of TokenBlockList
@@ -19,6 +21,9 @@ class BlockedToken(db.Model):
         """
         self.token = token
         self.blocked_on = datetime.utcnow()
+
+        payload = decode_jwt_token(token)
+        self.expiry = payload['exp']
 
     def __repr__(self):
         """String representation
